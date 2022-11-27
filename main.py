632f75833772c3b0a89d69d4bfdcc2c05de7753f
Emily Bauer <63298612+greatEmily@users.noreply.github.com>
2022-11-27 01:55:28 +0000

    Parameters

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def hello():
-  print("Hello Emily!")
+def hello(name):
+  print(f"Hello {name}!")
 
 
-hello ()
+hello ("John")
+
+def add_numbers(num1, num2):
+  print(num1 + num2)
+
+add_numbers(4,8)
+
+def dog_info(age, name):
+  print(f"This dog's name is {name} and the age is {age}.")
+
+
+dog_info(4, "Bruce")
