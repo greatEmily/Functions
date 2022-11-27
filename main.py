8b1f971c66ae4b587d6a658ddfb0b3737ae29c45
Emily Bauer <63298612+greatEmily@users.noreply.github.com>
2022-11-27 02:00:06 +0000

    Returns

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-def hello(name):
-  print(f"Hello {name}!")
+def uppercase(text):
+  return text.upper()
 
 
-hello ("John")
-
-def add_numbers(num1, num2):
-  print(num1 + num2)
-
-add_numbers(4,8)
-
-def dog_info(age, name):
-  print(f"This dog's name is {name} and the age is {age}.")
-
-
-dog_info(4, "Bruce")
+print(uppercase("this is the string"))
